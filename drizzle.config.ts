5fd75b457ec988dad35cd99e77dde24ab454a6ca
GODDA <sela.3meng@gmail.com>
2024-03-21 03:20:17 +0700

    update env usage --passed

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,15 @@
 import type { Config } from "drizzle-kit";
+import { AppEnv } from "./lib/app.env";
 
 export default {
     schema: "./lib/schema.ts",
     out: "./lib/drizzle",
     driver: "mysql2",
     dbCredentials: {
-        host: process.env.DB_HOST || "127.0.0.1",
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME || "todo_list",
+        host: AppEnv.db_host,
+        user: AppEnv.db_user,
+        password: AppEnv.db_password,
+        database: AppEnv.db_name,
     },
     breakpoints: true,
 } satisfies Config;
--- lib/db-connection.ts
+++ lib/db-connection.ts
@@ -1,12 +1,13 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import * as schema from "./schema";
+import { AppEnv } from "./app.env";
 
 export const connection = await mysql.createConnection({
-    host: process.env.DB_HOST || "127.0.0.1",
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME || "todo_list",
+    host: AppEnv.db_host,
+    user: AppEnv.db_user,
+    password: AppEnv.db_password,
+    database: AppEnv.db_name,
     multipleStatements: true,
 });
 export const db = drizzle(connection, { schema, mode: "default" });
